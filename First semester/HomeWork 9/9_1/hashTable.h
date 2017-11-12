a0b324646f79bbb86dbe21b54cec1c9abcf8795f
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2017-11-12 16:58:23 +0300

    bug with createTable() found

--- First semester/HomeWork 9/9_1/hashTable.h
+++ First semester/HomeWork 9/9_1/hashTable.h
@@ -15,7 +15,7 @@ void add(const std::string& key, HashTable* table, int value = 0);
 
 void remove(const std::string& key, HashTable* table);
 
-int getValue(const std::string& key, HashTable* table);
+int& get(const std::string& key, HashTable* table);
 
 void getKeys(HashTable* table, std::vector<std::string>& keys);
 
