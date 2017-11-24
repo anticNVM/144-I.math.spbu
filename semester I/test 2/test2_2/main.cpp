a24849d20c94bf7fd5cef518802fa222da0701e3
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2017-11-24 14:59:57 +0300

    test2.2 v0.1 ready to release

--- semester I/test 2/test2_2/main.cpp
+++ semester I/test 2/test2_2/main.cpp
@@ -1,10 +1,62 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <list.h>
 
 using namespace std;
 
+bool isSymmetrical(List* list);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    ifstream fin("input.txt");
+    if (!fin.good()) {
+        cout << "ERROR: file not opened!" << endl;
+        return 1;
+    }
+
+    string numbers = "";
+    List* list = createList();
+    while ((getline(fin, numbers))) {
+        string tmpNum = "";
+        for (auto ch : numbers) {
+            if (ch != ' ' && ch != '\n') {
+                tmpNum += ch;
+            } else {
+                push(atoi(tmpNum.c_str()), list);
+                tmpNum = "";
+            }
+        }
+        push(atoi(tmpNum.c_str()), list);
+        tmpNum = "";
+
+        cout << "Numbers: ";
+        printList(list);
+        if (isSymmetrical(list)) {
+            cout << "True:  List of numbers is symmetrical" << endl;
+        } else {
+            cout << "False: List of numbers is not symmetrical" << endl;
+        }
+        cout << endl;
+
+        clearList(list);
+    }
+    fin.close();
+    deleteList(list);
+
+    cout << "kek" << endl;
     return 0;
 }
 
+bool isSymmetrical(List* list)
+{
+    int listSize = getSize(list);
+    bool isSym = true;
+    for (int i = 0; i < (listSize / 2); ++i) {
+        if (getValue(list, i) != getValue(list, listSize - i - 1)) {
+            isSym = false;
+            break;
+        }
+    }
+    return isSym;
+}
--- semester I/test 2/test2_2/test2_2.pro
+++ semester I/test 2/test2_2/test2_2.pro
@@ -3,5 +3,12 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    list.cpp
+
+HEADERS += \
+    list.h
+
+DISTFILES += \
+    input.txt
 
