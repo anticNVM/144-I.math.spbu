12619a39433f1a4dc8038420c1a451bbed36a48c
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2017-11-29 19:16:24 +0300

    some minor changes

--- first semester/HomeWork 10/10_1/main.cpp
+++ first semester/HomeWork 10/10_1/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <utility>
 
 using namespace std;
+
+void makeGraph(ifstream& fin, vector<vector<int>> graph);
 
 int main()
 {
     ifstream fin("input.txt");
-    int a = 0;
-    fin >> a;
-    cout << "Hello World!   " << a << endl;
+    fin.close();
     return 0;
 }
 
+void makeGraph(ifstream& fin, vector<vector<int>> graph)
+{
+    int n = 0;
+    int m = 0;
+    fin << n << m;
+    graph.resize(n);
+    for (int i = 0; i < m; ++i) {
+        graph[
+    }
+}
