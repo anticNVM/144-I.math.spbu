9b4708038826ad1aac635bf6174f31fb61e3bf30
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2017-12-21 12:51:31 +0300

    bugs fixed, user iterface improved, some other fixes

--- First semester/HomeWork 4/4_3/main.cpp
+++ First semester/HomeWork 4/4_3/main.cpp
@@ -1,5 +1,4 @@
 #include "sources.h"
-#include <iostream>
 #include <stdio.h>
 #include <string>
 
@@ -9,12 +8,15 @@ int main()
 {
     PhoneBook* myBook = createBook();
     const string filename = "telephoneDirectory.txt";
-    if (unload(myBook, filename)) {
-        return 1;
+    if (!unload(myBook, filename)) {
+        FILE* file = fopen(filename.c_str(), "w");
+        fclose(file);
+        unload(myBook, filename);
     }
     displayWelcome();
-    mainLoop(myBook);
+    mainLoop(myBook, filename);
 
+    deleteBook(myBook);
     return 0;
 }
 
--- First semester/HomeWork 4/4_3/sources.cpp
+++ First semester/HomeWork 4/4_3/sources.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstring>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -21,24 +22,37 @@ PhoneBook* createBook()
 {
     PhoneBook* newBook = new PhoneBook{};
     newBook->notations = new PhoneNotation*[newBook->maxSize]{};
+    for (int i = 0; i < newBook->maxSize; ++i) {
+        newBook->notations[i] = new PhoneNotation{};
+    }
     return newBook;
+}
+
+void deleteBook(PhoneBook* book)
+{
+    for (int i = 0; i < book->maxSize; ++i) {
+        delete book->notations[i];
+    }
+    delete[] book->notations;
+    delete book;
 }
 
 PhoneNotation* createNotation()
 {
     PhoneNotation* note = new PhoneNotation{};
-    cout << "<Creation new phone-notation>\n";
+    cout << "\n<Creation new phone-notation>\n";
     cout << "Enter a name: ";
     cin >> note->name;
     cout << "Enter a number: ";
     cin >> note->number;
+    cout << endl;
 
     return note;
 }
 
 bool addNotation(PhoneBook* book)
 {
-    // добвить проверку на уникальность
+    // мб добвить проверку на уникальность
     if (book->index < book->maxSize) {
         PhoneNotation* note = createNotation();
         book->notations[book->index] = note;
@@ -51,13 +65,12 @@ bool addNotation(PhoneBook* book)
 
 void printNotation(PhoneNotation* note)
 {
-    cout << setw(20) << note->name << setw(20) << note->number << endl;
+    cout << setw(15) << note->name << setw(15) << note->number << endl;
 }
 
 void printPhoneBook(PhoneBook* book)
 {
-    cout << "Current Phone Book: \n";
-    cout << setw(20) << "NAME" << setw(20) << "NUMBER: " << endl;
+    cout << setw(15) << "NAME" << setw(15) << "NUMBER" << endl;
     for (int i = 0; i < book->index; ++i) {
         printNotation(book->notations[i]);
     }
@@ -72,7 +85,7 @@ string findByNumber(const std::string& number, PhoneBook* book)
             return note->name;
         }
     }
-    return nullptr;
+    return "";
 }
 
 string findByName(const std::string& name, PhoneBook* book)
@@ -83,23 +96,27 @@ string findByName(const std::string& name, PhoneBook* book)
             return note->number;
         }
     }
-    return nullptr;
+    return "";
 }
 
 bool unload(PhoneBook* book, const string& filename)
 {
-    FILE* file = fopen(filename.c_str(), "r");
-    if (file == nullptr) {
+    ifstream fin(filename);
+    if (!fin.is_open()) {
         return false;
     }
     int i = 0;
     bool isName = true;
     string buffer = "";
-    while (fscanf(file, "%s", buffer.c_str()) != EOF) {
+    while (!fin.eof()) {
+        fin >> buffer;
+        if (buffer.size() == 0) {
+            break;
+        }
         if (isName) {
-            book->notations[i]->name = buffer;
+            book->notations[i]->name += buffer;
         } else {
-            book->notations[i]->number = buffer;
+            book->notations[i]->number += buffer;
             (book->index)++;
             i++;
         }
@@ -107,7 +124,7 @@ bool unload(PhoneBook* book, const string& filename)
         isName = !isName;
 
     }
-    fclose(file);
+    fin.close();
     return true;
 }
 
@@ -118,7 +135,7 @@ bool upload(PhoneBook* notes, const string& filename)
         return false;
     }
     for (int i = 0; i < notes->index; ++i) {
-        fprintf(file, "%20s - ", notes->notations[i]->number.c_str());
+        fprintf(file, "%20s ", notes->notations[i]->name.c_str());
         fprintf(file, "%10s\n", notes->notations[i]->number.c_str());
     }
     fclose(file);
@@ -131,25 +148,17 @@ char menu()
     printf("0 - exit \n");
     printf("1 - add note \n");
     printf("2 - print all notes \n");
-    printf("3 - find number by name \n");
-    printf("4 - find name bu bumber \n");
+    printf("3 - find name by number \n");
+    printf("4 - find number by name \n");
     printf("5 - save current data in file \n");
     printf(">>> ");
     char choice = 0;
-    scanf("%c", &choice);
+    cin >> choice;
+    cin.get();
     return choice;
 }
 
-//enum struct Choices {
-//    add,
-//    print,
-//    findNumber,
-//    findName,
-//    save,
-//    exit
-//};
-
-void mainLoop(PhoneBook* book)
+void mainLoop(PhoneBook* book, const string& filename)
 {
     char usersChoice = menu();
     while (usersChoice != '0') {
@@ -166,36 +175,39 @@ void mainLoop(PhoneBook* book)
             printPhoneBook(book);
             break;
         }
+        // name search
         case '3':
         {
             string number = "";
             printf("Please, enter a number to search: ");
             cin >> number;
             string name = findByNumber(number, book);
-            cout << name << endl;
+            cout << endl << "  " << ((name != "") ? name : "There is no such note :(");
+            cout << endl << endl;
             break;
         }
+        // number search
         case '4':
         {
             string name = "";
             printf("Please, enter a name to search: ");
             cin >> name;
-            string number = findByNumber(name, book);
-            cout << number << endl;
+            string number = findByName(name, book);
+            cout << endl << "  " << ((number != "") ? number : "There is no such note :(");
+            cout << endl << endl;
             break;
         }
         case '5':
         {
-            string filename = "";
             if (upload(book, filename)) {
-                printf("Notes successfully saved to \'%s\' \n", filename.c_str());
+                printf("\nNotes successfully saved to \'%s\' \n\n", filename.c_str());
             } else {
-                printf("ERROR: \'%s\' cannot be opened. \n", filename.c_str());
+                printf("\nERROR: \'%s\' cannot be opened. \n\n", filename.c_str());
             }
             break;
         }
         default:
-            printf("Wrong input. Please, try again \n");
+            printf("Wrong input. Please, try again \n\n");
             break;
         }
         usersChoice = menu();
@@ -204,5 +216,6 @@ void mainLoop(PhoneBook* book)
 
 void displayWelcome()
 {
-    printf("kek \n");
+    cout << "...Welcome to the Telephone Dictionary...\n";
+    cout << "Please, choose one of following items from MENU: \n\n";
 }
