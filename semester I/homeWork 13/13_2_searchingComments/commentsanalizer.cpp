32ab1f7f50cfd36af5eab595a4c32dc3ad2451bf
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2017-12-28 16:59:40 +0300

    read table from file added

--- semester I/homeWork 13/13_2_searchingComments/commentsanalizer.cpp
+++ semester I/homeWork 13/13_2_searchingComments/commentsanalizer.cpp
@@ -5,13 +5,27 @@ int status(char ch);
 
 void stateMachine(FILE* file)
 {
-    int stateTable[4][3] = {
-     /* 's' '/' '*'     */
-        {0,  1,  0},    // read symbols, not a comment
-        {0,  1,  2},    // read '/' (means begin of comment, if next symbol is '*')
-        {2,  2,  3},    //`read comment
-        {2,  0,  3}     // read '*' (means end of comment, if next symbol is '/')
-    };
+    int amountOfStates = 0;
+    fscanf(file, "%d", &amountOfStates);
+    int amountOfTokens = 0;
+    fscanf(file, "%d", &amountOfTokens);
+    int** stateTable = new int*[amountOfStates];
+    for (int i = 0; i < amountOfStates; ++i) {
+        stateTable[i] = new int[amountOfTokens];
+    }
+    for (int i = 0; i < amountOfStates; ++i) {
+        for (int j = 0; j < amountOfTokens; ++j) {
+            fscanf(file, "%d", &stateTable[i][j]);
+        }
+    }
+
+//    int stateTable[4][3] = {
+//     /* 's' '/' '*'     */
+//        {0,  1,  0},    // read symbols, not a comment
+//        {0,  1,  2},    // read '/' (means begin of comment, if next symbol is '*')
+//        {2,  2,  3},    //`read comment
+//        {2,  0,  3}     // read '*' (means end of comment, if next symbol is '/')
+//    };
 
     int currentState = 0;
     int previousState = 0;
@@ -30,6 +44,10 @@ void stateMachine(FILE* file)
         }
         token = fgetc(file);
     }
+    for (int i = 0; i < amountOfStates; ++i) {
+        delete[] stateTable[i];
+    }
+    delete[] stateTable;
 }
 
 int status(char ch)
